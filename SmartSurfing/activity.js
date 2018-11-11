b392a281121dbc2b3362b5ad1beca6bb07d1292a
Sunny Qi <sunny_qi@Sunnys-MacBook-Pro-2.local>
2018-11-10 22:55:39 -0500

    added most visted sites table

--- SmartSurfing/activity.js
+++ SmartSurfing/activity.js
@@ -1,20 +1,40 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
+var siteHits = new Object();
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+chrome.history.search({text: ''}, function(data) {
+    for(var hit in data) {
+        var link = document.createElement("a")
+        link.href = data[hit].url
+
+        var hostname = link.hostname;
+        if(siteHits[hostname]) {
+            siteHits[hostname] += data[hit].visitCount;
+        } else {
+            siteHits[hostname] = data[hit].visitCount;
+        }
+    }
+
+    var orderedSiteHits = [];
+    for (var hit in siteHits) {
+        if(hit) {
+            orderedSiteHits.push([hit, siteHits[hit]]);
+        }
+    }
+    console.log(orderedSiteHits);
+    orderedSiteHits.sort(function(a, b) {
+        return a[1] - b[1];
     });
-};
+    console.log(orderedSiteHits);
+    var tbody = document.getElementById('siteVisits');
+
+    for (var i = orderedSiteHits.length - 1; i >= orderedSiteHits.length - 11; i-- ) {
+        var tr = "<tr>";
+
+        /* Must not forget the $ sign */
+        tr += "<td>" + orderedSiteHits[i][0] + "</td>" + "<td>" + orderedSiteHits[i][1] + "</td></tr>";
+
+        /* We add the table row to the table body */
+        tbody.innerHTML += tr;
+    }
+});
